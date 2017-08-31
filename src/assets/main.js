d0365d065684c344170a452a56224c5bf97cfe95
Erin <eaw.wright@gmail.com>
2017-08-31 09:33:52 -0400

    Created Ajax Call

--- src/assets/main.js
+++ src/assets/main.js
@@ -2,4 +2,13 @@ $(function() {
 
   // your code will go here
 
+  $.ajax({
+    url: 'https://www.codeschool.com/users/iistudio.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      // handle response
+    }
+  });
+
+
 });
